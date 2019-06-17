1780b88cdc2d58c0e4d4d79b49088b53449c9a12
Your Name <you@example.com>
2019-06-17 18:02:36 +0800

    mq

--- auth-service/src/main/java/com/morgana/auth/config/OAuth2AuthorizationServerConfig.java
+++ auth-service/src/main/java/com/morgana/auth/config/OAuth2AuthorizationServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -54,8 +55,9 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(redisTokenStore())
                  .accessTokenConverter(accessTokenConverter())
-                 .authenticationManager(authenticationManager)
-                 .reuseRefreshTokens(false);
+                 .authenticationManager(authenticationManager).reuseRefreshTokens(false)
+                 .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+
     }
 
     @Bean
